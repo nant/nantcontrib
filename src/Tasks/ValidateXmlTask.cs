35b04963ecbf7703ddf08dbccc2a0bf12d3c83af
Tomas Restrepo <tomasr@users.sourceforge.net>
2002-10-03 03:14:56 +0000

    Improved task by adding specialized schemas element handling class

--- src/Tasks/ValidateXmlTask.cs
+++ src/Tasks/ValidateXmlTask.cs
@@ -36,7 +36,7 @@ namespace NAnt.Contrib.Tasks
 
    /// <summary>
    /// A task that Validates a set of XML files based on a
-   /// set of Schemas (XSD, XDR and DTD)
+   /// set of Schemas (XSD)
    /// </summary>
    /// <remarks>
    /// This task takes a set of input xml files in a fileset
@@ -52,10 +52,11 @@ namespace NAnt.Contrib.Tasks
    ///   <code><![CDATA[
    ///      <validatexml>
    ///         <schemas>
-   ///            <option name="" value="rcf-schema.xsd"/>
+   ///            <schemaref source="rcf-schema.xsd"/>
+   ///            <schemaref namespace="urn:schemas-company-com:base" source="base-schema.xsd"/>
    ///          </schemas>
    ///         <xmlfiles>
-   ///            <includes name="rcf_example.xml"/>
+   ///            <includes name="*.xml"/>
    ///         </xmlfiles>
    ///      </validatexml>
    ///   ]]></code>
@@ -64,19 +65,19 @@ namespace NAnt.Contrib.Tasks
    public class ValidateXmlTask : Task
    {
       private FileSet _xmlFiles = new FileSet();
-      private OptionSet _schemas = new OptionSet();
       private int _numErrors = 0;
+      private SchemaSet _schemas = new SchemaSet();
 
       /// <summary>
       /// Set of XML files to use as input
       /// </summary>
-      [FileSet("xmlfiles")]
+      [FileSet("xmlfiles", Required=true)]
       public FileSet XmlFiles {
          get { return _xmlFiles; }
       }
 
-      [OptionSetAttribute("schemas")]
-      public OptionSet Schemas {
+      [SchemaSetAttribute("schemas", Required=true)]
+      public SchemaSet Schemas {
          get { return _schemas; }
       }
 
@@ -91,6 +92,11 @@ namespace NAnt.Contrib.Tasks
          if (XmlFiles.FileNames.Count == 0) {
             throw new BuildException("ValidateXml fileset cannot be empty!", Location);
          }
+
+         if ( _schemas.Schemas.Count == 0 ) {
+            throw new BuildException("ValidateXml at least one schema must be specified", Location);
+         }
+
       }
 
       /// <summary>
@@ -101,7 +107,11 @@ namespace NAnt.Contrib.Tasks
          foreach ( string file in XmlFiles.FileNames ) {
             Log.WriteLine(LogPrefix + "Validating " + file);
             Log.Indent();
+            try {
                ValidateFile(file);
+            } catch ( XmlException ex ) {
+               throw new BuildException("Invalid XML file: " + ex.Message, Location);
+            }
             Log.Unindent();
 
             if ( _numErrors == 0 ) {
@@ -116,13 +126,8 @@ namespace NAnt.Contrib.Tasks
       {
          XmlReader xmlReader = new XmlTextReader(file);
          XmlValidatingReader valReader = new XmlValidatingReader(xmlReader);
-         
-         foreach ( OptionValue schema in Schemas ) {
-            if ( schema.Name == "" ) 
-               valReader.Schemas.Add(null, schema.Value);
-            else
-               valReader.Schemas.Add(schema.Name, schema.Value);
-         }
+
+         valReader.Schemas.Add(_schemas.Schemas);
 
          valReader.ValidationEventHandler += 
             new ValidationEventHandler(OnValidationError);
@@ -142,5 +147,93 @@ namespace NAnt.Contrib.Tasks
 
 
    } // class ValidateXmlTask
+
+
+
+    /// <summary>Indicates that field should be treated as a xml schema set for the task.</summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited=true)]
+    public class SchemaSetAttribute : BuildElementAttribute 
+    {     
+
+        public SchemaSetAttribute(string name) : base(name) 
+        {        
+        }      
+    }
+
+   /// <summary>
+   /// Represents the schema collection element
+   /// </summary>
+   public class SchemaSet : Element
+   {
+      private XmlSchemaCollection _schemas = null;
+      
+      /// <summary>
+      /// Schemas in this element
+      /// </summary>
+      public XmlSchemaCollection Schemas {
+         get { return _schemas; }
+      }
+
+      /// <summary>
+      /// Initialize this element node
+      /// </summary>
+      /// <param name="elementNode"></param>
+      protected override void InitializeElement(XmlNode elementNode)  
+      {
+         try
+         {
+            //
+            // Check out whatever <schemaref> elements there are
+            //
+            _schemas = new XmlSchemaCollection();
+            foreach ( XmlNode node in elementNode ) 
+            {
+               if ( node.Name.Equals("schemaref") ) {
+                  SchemaRefElement v = new SchemaRefElement();
+                  v.Project = Project;
+                  v.Initialize(node);
+                  _schemas.Add(v.Namespace, v.Source);
+               }
+            }
+         } catch ( XmlSchemaException xse ) {
+            throw new BuildException("Invalid Schema: " + xse.Message, Location);
+         } 
+      }
+
+   } // class SchemaSet
+
+
+   /// <summary>
+   /// Allows the specification of a namespace/schema location pair
+   /// </summary>
+   [ElementName("schemaref")]
+   public class SchemaRefElement : Element
+   {
+      private string _namespace = null;
+      private string _source = null;
+
+      /// <summary>
+      /// Namespace URI associated with this schema. 
+      /// If not present, it is assumed that the 
+      /// schema's targetNamespace value is to be used.
+      /// </summary>
+      [TaskAttribute("namespace")]
+      public string Namespace {
+         get { return _namespace; }
+         set { _namespace = value; }
+      }
+
+      /// <summary>
+      /// Location of this schema. Could be a 
+      /// local file path or an http URL.
+      /// </summary>
+      [TaskAttribute("source", Required=true)]
+      public string Source {
+         get { return _source; }
+         set { _source = value; }
+      }
+
+   } // class SchemaRefElement
+
 
 } // namespace NAnt.Contrib.Tasks
