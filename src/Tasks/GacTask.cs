c9d93bae7d358b76125f580f7002d3176dd8694c
Ian MacLean <ianm74@users.sourceforge.net>
2003-04-16 16:56:27 +0000

    patch from Arjen Poutsma - Adds fileset functionality to GacUtil

--- src/Tasks/GacTask.cs
+++ src/Tasks/GacTask.cs
@@ -48,24 +48,38 @@ namespace NAnt.Contrib.Tasks {
 	/// </example>
     [TaskName("gac")]
     public class GacTask : ExternalProgramBase {
+        public enum ActionTypes {
+            install,
+            overwrite,
+            uninstall };
 
+        ActionTypes _action = ActionTypes.install;
         string _assemblyName = null;
-        bool _uninstall = false;
         int _timeout = Int32.MaxValue;
+        FileSet _assemblies = new FileSet();
+        bool _silent = false;
 
         /// <summary>The name of a file that contains an assembly manifest.</summary>
-        [TaskAttribute("assembly", Required=true)]
+        [TaskAttribute("assembly")]
         public string AssemblyName {
             get { return _assemblyName; }
             set { _assemblyName = value; }
         }
 
-        /// <summary>Removes the assembly from the global assembly cache and the native image cache.  Default is "false".</summary>
-        [TaskAttribute("uninstall")]
+        /// <summary>Defines the action to take with the assembly. Supported actions are: <c>install</c>, <c>overwrite</c>, and <c>uninstall</c>.</summary>
+        [TaskAttribute("action")]
+        public ActionTypes ActionType { get { return _action; } set { _action = value; }}
+
+        /// <summary>Fileset are used to define multiple assemblies.</summary>
+        [FileSet("assemblies")]
+        public FileSet CopyFileSet      { get { return _assemblies; } }
+
+        /// <summary>Quiet mode.</summary>
+        [TaskAttribute("silent")]
         [BooleanValidator()]
-        public bool Uninstall {
-            get { return _uninstall; }
-            set { _uninstall = value; }
+        public bool Silent {
+            get { return _silent; }
+            set { _silent = value; }
         }
 
         /// <summary>Stop the build if the command does not finish within the specified time.  Specified in milliseconds.  Default is no time out.</summary>
@@ -82,24 +96,59 @@ namespace NAnt.Contrib.Tasks {
 
         public override string ProgramArguments {
             get {
-                string assemblyName = AssemblyName;
-                if (Uninstall) {
-                    int dllExtension = assemblyName.ToUpper().IndexOf(".DLL");
-                    if (dllExtension > -1) {
-                        assemblyName = assemblyName.Substring(0, dllExtension);
-                    }
-                    return "/u " + assemblyName;
-                } else {
-                    return "/i " + assemblyName;
+                string prefix = Silent ? "/silent" : "/nologo";
+                switch (ActionType) {
+                    case ActionTypes.install:
+                        return String.Format("{0} /i {1}", prefix, AssemblyName);
+                    case ActionTypes.overwrite:
+                        return String.Format("{0} /if {1}", prefix, AssemblyName);
+                    case ActionTypes.uninstall:
+                        // uninstalling does not work with a filename, but with an assemblyname
+                        string assembly = null;
+                        FileInfo fi = new FileInfo(AssemblyName);
+                        if (fi.Exists) {
+                            // strip of the path and extension
+                            assembly = fi.Name.Substring(0, (fi.Name.Length - fi.Extension.Length));
+                        }
+                        return String.Format("{0} /u {1}", prefix, assembly);
+                    default:
+                        return "";
                 }
             }
         }
 
         protected override void ExecuteTask() {
-            // If the user wants to see the actual command the -verbose flag
-            // will cause ExternalProgramBase to display the actual call.
-            Log.WriteLine(LogPrefix + "{0} {1}", Uninstall ? "Uninstalling" : "Installing", AssemblyName);
-            base.ExecuteTask();
+            if ( (_assemblyName != null) && (_assemblies.FileNames.Count != 0)) {
+                throw new BuildException("Cannot use both the \"assembly\" attribute and a \"assemblies\" fileset");
+            } else if ( (_assemblyName == null) && (_assemblies.FileNames.Count == 0)) {
+                throw new BuildException("Specify either an \"assembly\" attribute or a \"assemblies\" fileset");
+            }
+            string msg = "";
+            switch (ActionType) {
+                case ActionTypes.install:
+                    msg = "Installing";
+                    break;
+                case ActionTypes.overwrite:
+                    msg = "Overwriting";
+                    break;
+                case ActionTypes.uninstall:
+                    msg = "Uninstalling";
+                    break;
+            }
+            if (_assemblies.FileNames.Count != 0) {
+                //multiple assemblies
+                Log.WriteLine(LogPrefix + "{0} {1} assemblies", msg, _assemblies.FileNames.Count);
+                foreach (string assemblyName in _assemblies.FileNames) {
+                    AssemblyName = assemblyName;
+                    base.ExecuteTask();
+                }
+            } else {
+
+                // If the user wants to see the actual command the -verbose flag
+                // will cause ExternalProgramBase to display the actual call.
+                Log.WriteLine(LogPrefix + "{0} {1}", msg, AssemblyName);
+                base.ExecuteTask();
+            }
         }
     }
 }
