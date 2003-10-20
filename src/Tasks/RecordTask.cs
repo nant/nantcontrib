f41c35a17e7f78abdd9e33a8045b20651446d492
drieseng <gert.driesen@telenet.be>
2003-10-20 18:48:28 +0000

    code restructure, xml doc updates, expose enums instead of using strings

--- src/Tasks/RecordTask.cs
+++ src/Tasks/RecordTask.cs
@@ -1,4 +1,3 @@
-//
 // NAntContrib
 // Copyright (C) 2002 Tomas Restrepo (tomasr@mvps.org)
 //
@@ -15,81 +14,102 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-//
 
 using System;
 using System.Collections;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Text;
-using NAnt.Core.Attributes;
+
 using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Util;
 
 using NAnt.Contrib.Util; 
 
-namespace NAnt.Contrib.Tasks
-{
-
+namespace NAnt.Contrib.Tasks {
     /// <summary>
-    /// A task that records the build's output
-    /// to a file. Loosely based on Ant's Record task.
+    /// A task that records the build's output to a file. Loosely based on Ant's 
+    /// <a href="http://ant.apache.org/manual/CoreTasks/recorder.html">Record</a>
+    /// task.
     /// </summary>
     /// <remarks>
-    /// This task allows you to record the build's output,
-    /// or parts of it to a file. You can start and stop
-    /// recording at any place in the build process.
+    /// This task allows you to record the build's output, or parts of it to a 
+    /// file. You can start and stop recording at any place in the build process.
     /// </remarks>
     /// <example>
-    ///   <code><![CDATA[
-    ///      <record name="${outputdir}\Buildlog.txt" level="info" action="Start"/>
-    ///      <record name="${outputdir}\Buildlog.txt" action="Close"/>
-    ///   ]]></code>
+    ///   <code>
+    ///     <![CDATA[
+    /// <record name="${outputdir}\Buildlog.txt" level="Info" action="Start"/>
+    /// <record name="${outputdir}\Buildlog.txt" action="Close"/>
+    ///     ]]>
+    ///   </code>
     /// </example>
     [TaskName("record")]
     public class RecordTask : Task {
         //
         // valid actions
         //
-        enum Actions {
+        public enum ActionType {
             Start = 0,
             Stop = 1,
             Close = 2,
             Flush = 3
         }
 
+        #region Private Instance Fields
+
         private string _logname;
-        private Actions _action;
-        static RecorderCollection _recorders = new RecorderCollection();
+        private ActionType _actionType;
         private bool _autoFlush = false;
         private Level _thresholdLevel = Level.Info;
-        private bool _autoFlushSet = false;
-        private bool _thresholdLevelSet = false;
+
+        #endregion Private Instance Fields
+
+        #region Private Static Fields
+
+        private static RecorderCollection _recorders = new RecorderCollection();
+
+        #endregion Private Static Fields
+
+        #region Public Instance Properties
 
         /// <summary>
-        /// Name of Destination file.
+        /// Name of destination file.
         /// </summary>
         [TaskAttribute("name", Required=true)]
+        [StringValidator(AllowEmpty=false)]
         public string LogName {
-            get { return _logname; }
-            set { _logname = value; }
+            get { return (_logname != null) ? Project.GetFullPath(_logname) : null; }
+            set { _logname = StringUtils.ConvertEmptyToNull(value); }
         }    
 
         /// <summary>
-        /// Action to apply to this log instance
+        /// Action to apply to this log instance - either <see cref="ActionType.Start" />,
+        /// <see cref="ActionType.Stop" />, <see cref="ActionType.Close" /> or
+        /// <see cref="ActionType.Flush" />.
         /// </summary>
-        /// <remarks>It can take on of the following values: start, stop, close, flush</remarks>
         [TaskAttribute("action", Required=true)]
-        public string Action {
-            get { return _action.ToString(); }
-            set { _action = (Actions)Enum.Parse(typeof(Actions), value); }
+        public ActionType Action {
+            get { return _actionType; }
+            set {
+                if (!Enum.IsDefined(typeof(ActionType), value)) {
+                    throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, 
+                        "An invalid action {0} was specified.", value)); 
+                } else {
+                    this._actionType = value;
+                }
+            } 
         }
 
         /// <summary>
-        /// Determines whether the recorder will flush it's buffer
-        /// after every write to it.
-        /// Default value is false
+        /// Determines whether the recorder will flush it's buffer after every 
+        /// write to it. The default is <see langword="false" />.
         /// </summary>
-        /// <remarks>Effective only with the start action</remarks>
+        /// <remarks>
+        /// Effective only with the <see cref="ActionType.Start" /> action.
+        /// </remarks>
         [TaskAttribute("autoflush", Required=false)]
         [BooleanValidator()]
         public bool AutoFlush {
@@ -98,76 +118,88 @@ namespace NAnt.Contrib.Tasks
         }
 
         /// <summary>
-        /// Determine the level of logging. default value is <b>info</b>
+        /// Determine the level of logging - either <see cref="Level.Debug" />, 
+        /// <see cref="Level.Verbose" />, <see cref="Level.Info" />, 
+        /// <see cref="Level.Warning" /> or <see cref="Level.Error" />. 
+        /// The default is <see cref="Level.Info" />.
         /// </summary>
-        /// <remarks>It can takes the following values: error, warning, info, verbose, debug</remarks>
-        /// <remarks>Effective only with the start action</remarks>
+        /// <remarks>
+        /// Effective only with the <see cref="ActionType.Start" /> action.
+        /// </remarks>
         [TaskAttribute("level", Required=false)]
-        public string ThresholdLevel {
-             get{ return this._thresholdLevel.ToString(); }
-             set{ this._thresholdLevel = (Level)Enum.Parse(typeof(Level), value); }
+        public Level ThresholdLevel {
+            get { return this._thresholdLevel; }
+            set {
+                if (!Enum.IsDefined(typeof(Level), value)) {
+                    throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, 
+                        "An invalid level {0} was specified.", value)); 
+                } else {
+                    this._thresholdLevel = value;
+                }
+            }
         }
 
+        #endregion Public Instance Properties
 
-        ///<summary>
-        ///Initializes task and ensures the supplied attributes are valid.
-        ///</summary>
-        ///<param name="taskNode">Xml node used to define this task instance.</param>
-        protected override void InitializeTask(System.Xml.XmlNode taskNode) {
-           if (LogName == null) {
-              throw new BuildException("Record attribute \"name\" is required.", Location);
-           }
-           if (Action == null) {
-              throw new BuildException("Record attribute \"action\" is required.", Location);
-           }
-              _autoFlushSet = (taskNode.Attributes["autoflush"] != null);
-              _thresholdLevelSet = (taskNode.Attributes["level"] != null);
+        #region Private Static Properties
+
+        private static RecorderCollection Recorders {
+            get { return _recorders; }
         }
+
+        #endregion Private Static Properties
+
+        #region Override implementation of Task
 
         /// <summary>
-        /// This is where the work is done
+        /// This is where the work is done.
         /// </summary>
         protected override void ExecuteTask() {
-            IRecorder recorder = _recorders.GetRecorder(LogName);
+            IRecorder recorder = _recorders[LogName];
 
-            switch ( _action ) {
-            case Actions.Start:
-                if ( recorder == null ) {
-                   recorder = new FileLogListener(LogName);
-                   _recorders.AddRecorder(recorder);
-                }
-                if(_autoFlushSet) {
-                     recorder.AutoFlush = _autoFlush;
-                }
-                if(_thresholdLevelSet) {
-                     recorder.Logger.Threshold = _thresholdLevel;
-                }
-                recorder.Start();
-                AttachRecorder(recorder);
-                break;
-            case Actions.Stop:
-                if ( recorder == null ) {
-                   throw new BuildException("Tried to stop non-existent recorder");
-                }
-                DetachRecorder(recorder);
-                recorder.Stop();
-                break;
-            case Actions.Close:
-                if ( recorder == null ) {
-                   throw new BuildException("Tried to close non-existent recorder");
-                }
-                DetachRecorder(recorder);
-                recorder.Close();
-                _recorders.RemoveRecorder(recorder.Name);
-                break;
-            case Actions.Flush:
-                if ( recorder == null )
-                   throw new BuildException("Tried to flush non-existent recorder");
-                recorder.Flush();
-                break;
+            switch (Action) {
+                case ActionType.Start:
+                    if (recorder == null) {
+                        recorder = new FileLogListener(LogName);
+                        Recorders.Add(recorder);
+                    }
+                    recorder.AutoFlush = AutoFlush;
+                    recorder.Logger.Threshold = ThresholdLevel;
+                    recorder.Start();
+                    AttachRecorder(recorder);
+                    break;
+                case ActionType.Stop:
+                    if (recorder == null) {
+                        throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                            "Tried to stop non-existent recorder '{0}'", LogName), 
+                            Location);
+                    }
+                    recorder.Stop();
+                    break;
+                case ActionType.Close:
+                    if (recorder == null) {
+                        throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                            "Tried to close non-existent recorder '{0}'", LogName),
+                            Location);
+                    }
+                    DetachRecorder(recorder);
+                    recorder.Close();
+                    Recorders.Remove(recorder.Name);
+                    break;
+                case ActionType.Flush:
+                    if (recorder == null) {
+                        throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                            "Tried to flush non-existent recorder '{0}'", LogName),
+                            Location);
+                    }
+                    recorder.Flush();
+                    break;
             }
-
         }
+
+        #endregion Override implementation of Task
+
+        #region Private Instance Methods
 
         private void DetachRecorder(IRecorder recorder) {
             IBuildLogger logger = recorder.Logger;
@@ -181,8 +213,9 @@ namespace NAnt.Contrib.Tasks
             Project.TaskFinished -= new BuildEventHandler(logger.TaskFinished);
             Project.MessageLogged -= new BuildEventHandler(logger.MessageLogged);
 
-            if(Project.BuildListeners.Contains(logger))
+            if (Project.BuildListeners.Contains(logger)) {
                 Project.BuildListeners.Remove(logger);
+            }
         }
 
         private void AttachRecorder(IRecorder recorder) {
@@ -201,6 +234,7 @@ namespace NAnt.Contrib.Tasks
 
             Project.BuildListeners.Add(logger);
         }
-   } // class RecordTask
 
-} // namespace NAnt.Contrib.Tasks
+        #endregion Private Instance Methods
+    }
+}
