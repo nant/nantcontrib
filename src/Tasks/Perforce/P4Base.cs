a7d0ceafd3ecf3f285f4a971c3d0e224f1b5e4a3
Ian MacLean <ianm74@users.sourceforge.net>
2003-12-07 06:22:45 +0000

    first check in of new perforce task: p4sync. More p4 tasks to come.

---
+++ src/Tasks/Perforce/P4Base.cs
@@ -0,0 +1,160 @@
+// NAnt - A .NET build tool
+// Copyright (C) 2001-2002 Gerry Shaw
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Ian MacLean ( ian_maclean@another.com )
+
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using NAnt.Core.Tasks;
+using NAnt.Core.Util;
+using NAnt.Core.Attributes;
+
+namespace NAnt.Contrib.Tasks.Perforce {
+
+    /// <summary>Base class for Perforce (P4) NAnt tasks. See individual task for example usage.
+    /// </summary>
+    /// <seealso cref="P4Sync">P4Sync
+    /// </seealso>
+    /// <seealso cref="P4Sync">P4Have
+    /// </seealso>
+    /// <seealso cref="P4Sync">P4Change
+    /// </seealso>
+    /// <seealso cref="P4Sync">P4Edit
+    /// </seealso>
+    /// <seealso cref="P4Submit">P4Submit
+    /// </seealso>
+    /// <seealso cref="P4Sync">P4Label
+    /// </seealso>   
+    public abstract class P4Base: ExternalProgramBase {
+        
+        #region Private Instance Fields
+        
+        string _arguments = null;
+        private string _perforcePort = null;
+        private string _perforceClient = null;
+        private string _perforceUser = null;
+        private string _perforceView = null;
+       
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
+        
+        //private string _perforceCommandOptions = "";
+        
+        /// <summary> The p4d server and port to connect to;
+        /// optional, default "perforce:1666"
+        /// </summary>
+        [TaskAttribute("port")]
+        public string Port {
+            get { return _perforcePort; }
+            set {_perforcePort = StringUtils.ConvertEmptyToNull(value); }
+
+        }
+        /// <summary> The p4 client spec to use;
+        /// optional, defaults to the current user
+        /// </summary>
+        [TaskAttribute("client")]
+        public string Client {
+            get { return _perforceClient; }
+            set { _perforceClient = StringUtils.ConvertEmptyToNull(value); }
+
+        }
+        /// <summary> The p4 username;
+        /// optional, defaults to the current user
+        /// </summary>
+        [TaskAttribute("user")]
+        public string User {
+            get { return _perforceUser; }
+            set { _perforceUser = StringUtils.ConvertEmptyToNull(value); }
+
+        }
+        /// <summary> The client, branch or label view to operate upon;
+        /// optional default "//..."
+        /// </summary>
+        [TaskAttribute("view")]
+        virtual public string View {
+            get { return _perforceView; }
+            set { _perforceView = StringUtils.ConvertEmptyToNull(value); }
+
+        }
+        ///// <summary> Set extra command options; only used on some
+        ///// of the Perforce tasks.
+        ///// </summary>
+        //[TaskAttribute("options")]
+        //virtual public string CommandOptions {
+        //    get { return _perforceCommandOptions; }
+        //    set { _perforceCommandOptions = StringUtils.ConvertEmptyToNull(value); }
+        //}
+
+        /// <summary>
+        /// Gets the command line arguments for the external program.
+        /// </summary>
+        /// <value>
+        /// The command line arguments for the external program.
+        /// </value>
+        public override string ProgramArguments {
+            get { return _arguments; }
+        }
+
+        /// <summary>
+        /// Override the ExeName paramater for p4.exe
+        /// </summary>
+        public override string ExeName {
+            get { return "p4"; }
+        }
+            
+        #endregion Public Instance Properties
+        
+        /// <summary>
+        /// / Derived classes to override this
+        /// </summary>
+        protected abstract string CommandSpecificArguments {
+            get;
+        }
+        
+        #region Override implementation of Task
+        
+        /// <summary>
+        /// Execute the perforce command assembled by subclasses.
+        /// </summary>
+        protected override void ExecuteTask() {
+            StringBuilder arguments = new StringBuilder();
+
+            //perforce global options
+            if (Port != null) {
+                arguments.Append(string.Format(" -p {0}", Port ) );
+            }
+            if (User != null ) {
+                arguments.Append(string.Format(" -u {0}", User ) );
+            }
+            if (Client != null ) {
+                arguments.Append(string.Format(" -c {0}", Client ) );
+
+            }
+            // Get the command specific arguments from the derived class
+            arguments.Append(" ");
+            arguments.Append(CommandSpecificArguments);
+            
+            //arguments.Append(CommandOptions);     
+            _arguments = arguments.ToString();
+
+            // call base class to do perform the actual call
+            base.ExecuteTask();
+        } 
+        #endregion Override implementation of Task
+    }
+}
---
+++ src/Tasks/Perforce/P4Sync.cs
@@ -0,0 +1,122 @@
+// NAnt - A .NET build tool
+// Copyright (C) 2001-2002 Gerry Shaw
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Ian MacLean ( ian_maclean@another.com )
+
+using System;
+using System.Text;
+using NAnt.Core;
+using NAnt.Core.Util;
+using NAnt.Core.Tasks;
+using NAnt.Core.Attributes;
+
+namespace NAnt.Contrib.Tasks.Perforce {
+
+    /// <summary>Synchronize client space to a Perforce depot view.  
+    /// The API allows additional functionality of the "p4 sync" command
+    /// (such as "p4 sync -f //...#have" or other exotic invocations).
+    /// </summary>
+    ///<example>
+    ///<para>Sync to head using P4USER, P4PORT and P4CLIENT settings specified</para>    
+    ///<code>
+    ///     <![CDATA[
+    ///<p4Sync view="//projects/foo/main/source/..." user="fbloggs" port="km01:1666" client="fbloggsclient" />
+    ///     ]]>
+    ///   </code>
+    ///<para>Sync to head using default p4 environment variables</para>    
+    ///<code>
+    ///     <![CDATA[
+    ///<p4sync p4view="//projects/foo/main/source/..." />
+    ///     ]]>
+    ///   </code>    
+    ///<para>Force a re-sync to head, refreshing all files</para>
+    ///<code>
+    ///     <![CDATA[
+    ///<p4sync force="true" view="//projects/foo/main/source/..." />
+    ///     ]]>
+    ///   </code>
+    ///<para>Sync to a label</para>
+    ///<code>
+    ///     <![CDATA[
+    ///<p4-sync label="myPerforceLabel" />
+    ///     ]]>
+    ///   </code>
+    ///</example>
+    /// <todo> Add decent label error handling for non-exsitant labels</todo>    
+    [TaskName("p4sync")]
+    public class P4Sync : P4Base {
+        
+        #region Private Instance Fields
+        
+        private string _label = null;
+        private bool _force = false;
+        
+        #endregion Private Instance Fields
+        
+        #region Public Instance Properties
+        
+        /// <summary> Label to sync client to; optional.
+        /// </summary>
+        [TaskAttribute("label")]
+        public string Label {
+            get { return _label; }
+            set { _label = StringUtils.ConvertEmptyToNull(value); }
+        }       
+        
+        /// <summary> force a refresh of files, if this attribute is set; false by default.
+        /// </summary>
+        [TaskAttribute("force")]
+        [BooleanValidator()]
+        public bool Force {
+            set { _force = value;}
+            get { return _force; }
+        }
+        
+        #endregion Public Instance Properties
+        
+        /// <summary>
+        /// This is an override used by the base class to get command specific args.
+        /// </summary>
+        protected override string CommandSpecificArguments {
+            get { return getSpecificCommandArguments(); }
+        }
+        
+        #region Override implementation of Task
+        
+        /// <summary>
+        /// local method to build the command string for this particular command
+        /// </summary>
+        /// <returns></returns>
+        protected string getSpecificCommandArguments( ) {
+            StringBuilder arguments = new StringBuilder();
+            arguments.Append("-s sync ");
+            
+            if ( View  != null ) {
+                arguments.Append( View );
+            }
+            if ( Label  != null ) {
+                arguments.Append( string.Format("@{0}", Label ));
+            }
+            if ( Force ) {
+                arguments.Append( " -f");
+            }                       
+            //arguments.append("-s sync " + P4CmdOpts + " " + syncCmd);
+           return arguments.ToString();
+        }
+        
+        #endregion Override implementation of Task
+    }
+}
