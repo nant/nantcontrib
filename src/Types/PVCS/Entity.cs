c1c5282a8f4d44f848197daa0f2f3c39db2f94a3
drieseng <gert.driesen@telenet.be>
2004-10-06 05:28:06 +0000

    Types for PVCS tasks contributed by Kent Boogaart (thanks !!!)

---
+++ src/Types/PVCS/Entity.cs
@@ -0,0 +1,105 @@
+#region GNU General Public License
+
+// NAntContrib
+// Copyright (C) 2004 Kent Boogaart
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// Kent Boogaart (kentcb@internode.on.net)
+
+#endregion
+
+using System;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Util;
+
+namespace NAnt.Contrib.Types.PVCS {
+	/// <summary>
+	/// Represents an entity in an <see cref="EntitySet"/>.
+	/// </summary>
+	public sealed class Entity : Element {
+		#region Fields
+
+		/// <see cref="Name"/>
+		private string _name;
+
+		/// <see cref="If"/>
+		private bool _if;
+
+		/// <see cref="Unless"/>
+		private bool _unless;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// The path for the entity.
+		/// </summary>
+		[TaskAttribute("name", Required = true)]
+		[StringValidator(AllowEmpty = false)]
+		public new string Name {
+			get {
+				return _name;
+			}
+			set {
+				_name = value;
+			}
+		}
+
+		/// <summary>
+		/// If <c>true</c> then the entity will be included. The default is <c>true</c>.
+		/// </summary>
+		[TaskAttribute("if")]
+		[BooleanValidator]
+		public bool If {
+			get {
+				return _if;
+			}
+			set {
+				_if = value;
+			}
+		}
+
+		/// <summary>
+		/// Opposite of <see cref="If"/>. If <c>false</c> then the entity will be included. The default is
+		/// <c>false</c>.
+		/// </summary>
+		[TaskAttribute("unless")]
+		[BooleanValidator]
+		public bool Unless {
+			get {
+				return _unless;
+			}
+			set {
+				_unless = value;
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Constructs and initializes an instance of <c>Entity</c>.
+		/// </summary>
+		public Entity() {
+			If = true;
+		}
+
+		#endregion
+	}
+}
---
+++ src/Types/PVCS/EntitySet.cs
@@ -0,0 +1,82 @@
+#region GNU General Public License
+
+// NAntContrib
+// Copyright (C) 2004 Kent Boogaart
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// Kent Boogaart (kentcb@internode.on.net)
+
+#endregion
+
+using System;
+using System.Collections.Specialized;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace NAnt.Contrib.Types.PVCS {
+	/// <summary>
+	/// Represents a set of entities to include in a PVCS project database task.
+	/// </summary>
+	/// <seealso cref="NAnt.Contrib.Tasks.PVCS.PVCSMultipleEntityTask"/>
+	[Serializable]
+	[ElementName("entities")]
+	public sealed class EntitySet : DataTypeBase {
+		#region Fields
+
+		/// <see cref="EntityPaths"/>
+		private StringCollection _entityPaths;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the collection of entity paths assigned to this entity set.
+		/// </summary>
+		internal StringCollection EntityPaths {
+			get {
+				return _entityPaths;
+			}
+		}
+
+		/// <summary>
+		/// The entities to include in the project task.
+		/// </summary>
+		[BuildElementArray("entity", Required = true)]
+		public Entity[] Entities {
+			set {
+				foreach (Entity entity in value) {
+					if (entity.If && !entity.Unless) {
+						EntityPaths.Add(entity.Name);
+					}
+				}
+			}
+		}
+
+		#endregion
+		
+		#region Methods
+
+		/// <summary>
+		/// Constructs and initializes an instance of <c>EntitySet</c>.
+		/// </summary>
+		public EntitySet() {
+			_entityPaths = new StringCollection();
+		}
+
+		#endregion
+	}
+}
