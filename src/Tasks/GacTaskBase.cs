fff1a5522fc04b36aea9505aa97d2944c08b040f
Ian MacLean <ianm74@users.sourceforge.net>
2005-01-10 04:18:43 +0000

    Formatting fixes: - tabs to spaces - brace position etc - removed unused using declarations

--- src/Tasks/GacTaskBase.cs
+++ src/Tasks/GacTaskBase.cs
@@ -32,204 +32,204 @@ using NAnt.Core.Attributes;
 using NAnt.Contrib.Types;
 
 namespace NAnt.Contrib.Tasks {
-	/// <summary>
-	/// Base class functionality for the GAC tasks.
-	/// </summary>
-	/// <remarks>
-	/// Concrete GAC tasks extend this class in order to obtain common functionality.
-	/// </remarks>
-	public abstract class GacTaskBase : ExternalProgramBase {
-		#region Fields
+    /// <summary>
+    /// Base class functionality for the GAC tasks.
+    /// </summary>
+    /// <remarks>
+    /// Concrete GAC tasks extend this class in order to obtain common functionality.
+    /// </remarks>
+    public abstract class GacTaskBase : ExternalProgramBase {
+        #region Fields
 
-		/// <summary>
-		/// Stores the details of the assembly currently being operated against. This could be a name or
-		/// path, depending on the concrete task.
-		/// </summary>
-		private string _currentAssembly;
+        /// <summary>
+        /// Stores the details of the assembly currently being operated against. This could be a name or
+        /// path, depending on the concrete task.
+        /// </summary>
+        private string _currentAssembly;
 
-		/// <summary>
-		/// See <see cref="Force"/>.
-		/// </summary>
-		private bool _force;
+        /// <summary>
+        /// See <see cref="Force"/>.
+        /// </summary>
+        private bool _force;
 
-		/// <summary>
-		/// See <see cref="Reference"/>.
-		/// </summary>
-		private GacReference _reference;
+        /// <summary>
+        /// See <see cref="Reference"/>.
+        /// </summary>
+        private GacReference _reference;
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets a value indicating whether the GAC operation will be forced.
-		/// </summary>
-		/// <remarks>
-		/// The exact meaning of this property is dependent on the subclass. As such, subclasses should override this
-		/// property to provide a valid description.
-		/// </remarks>
-		[TaskAttribute("force", Required = false)]
-		[BooleanValidator]
-		public virtual bool Force {
-			get { return _force; }
-			set { _force = value; }
-		}
+        /// <summary>
+        /// Gets or sets a value indicating whether the GAC operation will be forced.
+        /// </summary>
+        /// <remarks>
+        /// The exact meaning of this property is dependent on the subclass. As such, subclasses should override this
+        /// property to provide a valid description.
+        /// </remarks>
+        [TaskAttribute("force", Required = false)]
+        [BooleanValidator]
+        public virtual bool Force {
+            get { return _force; }
+            set { _force = value; }
+        }
 
-		/// <summary>
-		/// Specifies reference details to use when working with the GAC.
-		/// </summary>
-		[BuildElement("reference", Required = false)]
-		public GacReference Reference {
-			get { return _reference; }
-			set { _reference = value; }
-		}
+        /// <summary>
+        /// Specifies reference details to use when working with the GAC.
+        /// </summary>
+        [BuildElement("reference", Required = false)]
+        public GacReference Reference {
+            get { return _reference; }
+            set { _reference = value; }
+        }
 
-		/// <summary>
-		/// Concrete GAC tasks must override this property to return an array of assembly names or paths
-		/// upon which to operate.
-		/// </summary>
-		protected abstract ICollection AssemblyList {
-			get;
-		}
+        /// <summary>
+        /// Concrete GAC tasks must override this property to return an array of assembly names or paths
+        /// upon which to operate.
+        /// </summary>
+        protected abstract ICollection AssemblyList {
+            get;
+        }
 
-		/// <summary>
-		/// Gets the executable name for the <c>gacutil</c> command-line tool.
-		/// </summary>
-		public sealed override string ExeName {
-			get { return "gacutil"; }
-		}
+        /// <summary>
+        /// Gets the executable name for the <c>gacutil</c> command-line tool.
+        /// </summary>
+        public sealed override string ExeName {
+            get { return "gacutil"; }
+        }
 
-		/// <summary>
-		/// Specifies whether a reference was specified for the GAC task.
-		/// </summary>
-		protected bool ReferenceSpecified {
-			get { return (Reference != null); }
-		}
+        /// <summary>
+        /// Specifies whether a reference was specified for the GAC task.
+        /// </summary>
+        protected bool ReferenceSpecified {
+            get { return (Reference != null); }
+        }
 
-		/// <summary>
-		/// Gets the current assembly being operated against.
-		/// </summary>
-		protected string CurrentAssembly {
-			get { return _currentAssembly; }
-		}
+        /// <summary>
+        /// Gets the current assembly being operated against.
+        /// </summary>
+        protected string CurrentAssembly {
+            get { return _currentAssembly; }
+        }
 
-		#endregion
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Constructs and initialises an instance of <c>GacTask</c>.
-		/// </summary>
-		public GacTaskBase() {
-		}
+        /// <summary>
+        /// Constructs and initialises an instance of <c>GacTask</c>.
+        /// </summary>
+        public GacTaskBase() {
+        }
 
-		/// <summary>
-		/// Gets the program arguments with which to run the <c>gacutil</c> process.
-		/// </summary>
-		public sealed override string ProgramArguments {
-			get {
-				StringBuilder retVal = new StringBuilder();
+        /// <summary>
+        /// Gets the program arguments with which to run the <c>gacutil</c> process.
+        /// </summary>
+        public sealed override string ProgramArguments {
+            get {
+                StringBuilder retVal = new StringBuilder();
 
-				//never show logo
-				retVal.Append("/nologo");
+                //never show logo
+                retVal.Append("/nologo");
 
-				if (!Verbose) {
-					retVal.Append(" /silent");
-				}
+                if (!Verbose) {
+                    retVal.Append(" /silent");
+                }
 
-				if (Force) {
-					retVal.Append(" /f");
-				}
+                if (Force) {
+                    retVal.Append(" /f");
+                }
 
-				if (ReferenceSpecified && Reference.If && !Reference.Unless) {
-					retVal.Append(" /r ");
+                if (ReferenceSpecified && Reference.If && !Reference.Unless) {
+                    retVal.Append(" /r ");
 
-					switch (Reference.SchemeType) {
-						case SchemeType.FilePath:
-							retVal.Append("FILEPATH");
-							break;
-						case SchemeType.Opaque:
-							retVal.Append("OPAQUE");
-							break;
-						case SchemeType.UninstallKey:
-							retVal.Append("UNINSTALL_KEY");
-							break;
-						default:
-							throw new BuildException("Unknown SchemeType: " + Reference.SchemeType);
-					}
+                    switch (Reference.SchemeType) {
+                        case SchemeType.FilePath:
+                            retVal.Append("FILEPATH");
+                            break;
+                        case SchemeType.Opaque:
+                            retVal.Append("OPAQUE");
+                            break;
+                        case SchemeType.UninstallKey:
+                            retVal.Append("UNINSTALL_KEY");
+                            break;
+                        default:
+                            throw new BuildException("Unknown SchemeType: " + Reference.SchemeType);
+                    }
 
-					retVal.Append(" \"").Append(Reference.SchemeId).Append("\" \"").Append(Reference.SchemeDescription).Append("\"");
-				}
+                    retVal.Append(" \"").Append(Reference.SchemeId).Append("\" \"").Append(Reference.SchemeDescription).Append("\"");
+                }
 
-				//allow concrete task to append any arguments
-				AppendProgramArguments(retVal);
-				//append the name of the assembly being operated against
-				retVal.Append(" \"").Append(CurrentAssembly).Append("\"");
+                //allow concrete task to append any arguments
+                AppendProgramArguments(retVal);
+                //append the name of the assembly being operated against
+                retVal.Append(" \"").Append(CurrentAssembly).Append("\"");
 
-				return retVal.ToString();
-			}
-		}
+                return retVal.ToString();
+            }
+        }
 
-		/// <summary>
-		/// Starts the process that is wrapped by this GAC task.
-		/// </summary>
-		/// <remarks>
-		/// Provided only to seal the implementation of <c>StartProcess()</c>.
-		/// </remarks>
-		/// <returns>The process that was started.</returns>
-		protected sealed override Process StartProcess() {
-			return base.StartProcess();
-		}
+        /// <summary>
+        /// Starts the process that is wrapped by this GAC task.
+        /// </summary>
+        /// <remarks>
+        /// Provided only to seal the implementation of <c>StartProcess()</c>.
+        /// </remarks>
+        /// <returns>The process that was started.</returns>
+        protected sealed override Process StartProcess() {
+            return base.StartProcess();
+        }
 
-		/// <summary>
-		/// Validates the task's configuration.
-		/// </summary>
-		/// <param name="taskNode">The task node.</param>
-		protected sealed override void InitializeTask(System.Xml.XmlNode taskNode) {
-			base.InitializeTask(taskNode);
+        /// <summary>
+        /// Validates the task's configuration.
+        /// </summary>
+        /// <param name="taskNode">The task node.</param>
+        protected sealed override void InitializeTask(System.Xml.XmlNode taskNode) {
+            base.InitializeTask(taskNode);
 
-			if (AssemblyList.Count == 0) {
-				throw new BuildException("At least one assembly must be specified.");
-			}
+            if (AssemblyList.Count == 0) {
+                throw new BuildException("At least one assembly must be specified.");
+            }
 
-			if (ReferenceSpecified) {
-				if (Reference.SchemeId == null) {
-					throw new BuildException("Must provide a schemeid when specifying a reference.");
-				}
-			}
-		}
+            if (ReferenceSpecified) {
+                if (Reference.SchemeId == null) {
+                    throw new BuildException("Must provide a schemeid when specifying a reference.");
+                }
+            }
+        }
 
-		/// <summary>
-		/// Executes the task.
-		/// </summary>
-		/// <remarks>
-		/// Provided only to seal the implementation of <c>ExecuteTask()</c>.
-		/// </remarks>
-		protected sealed override void ExecuteTask() {
-			foreach (string assembly in AssemblyList) {
-				_currentAssembly = assembly;
-				BeforeExecuteTask();
-				base.ExecuteTask();
-			}
-		}
+        /// <summary>
+        /// Executes the task.
+        /// </summary>
+        /// <remarks>
+        /// Provided only to seal the implementation of <c>ExecuteTask()</c>.
+        /// </remarks>
+        protected sealed override void ExecuteTask() {
+            foreach (string assembly in AssemblyList) {
+                _currentAssembly = assembly;
+                BeforeExecuteTask();
+                base.ExecuteTask();
+            }
+        }
 
-		/// <summary>
-		/// Appends any task-specific program arguments.
-		/// </summary>
-		/// <param name="sb">The <c>StringBuilder</c> on which to append program arguments.</param>
-		/// <remarks>
-		/// Subclasses must override this method to return the arguments with which to run the GAC task.
-		/// </remarks>
-		protected abstract void AppendProgramArguments(StringBuilder sb);
+        /// <summary>
+        /// Appends any task-specific program arguments.
+        /// </summary>
+        /// <param name="sb">The <c>StringBuilder</c> on which to append program arguments.</param>
+        /// <remarks>
+        /// Subclasses must override this method to return the arguments with which to run the GAC task.
+        /// </remarks>
+        protected abstract void AppendProgramArguments(StringBuilder sb);
 
-		/// <summary>
-		/// Invoked prior to invoking <c>ExecuteTask()</c> on the base class.
-		/// </summary>
-		/// <remarks>
-		/// Allows, for example, subclasses to output useful information to the log.
-		/// </remarks>
-		protected abstract void BeforeExecuteTask();
+        /// <summary>
+        /// Invoked prior to invoking <c>ExecuteTask()</c> on the base class.
+        /// </summary>
+        /// <remarks>
+        /// Allows, for example, subclasses to output useful information to the log.
+        /// </remarks>
+        protected abstract void BeforeExecuteTask();
 
-		#endregion
-	}
+        #endregion
+    }
 }
--- src/Tasks/GacUninstallTask.cs
+++ src/Tasks/GacUninstallTask.cs
@@ -31,186 +31,186 @@ using NAnt.DotNet.Types;
 using NAnt.Contrib.Types;
 
 namespace NAnt.Contrib.Tasks {
-	/// <summary>
-	/// Uninstalls assemblies from the Global Assembly Cache (GAC) by using the <c>gacutil</c> SDK tool.
-	/// </summary>
-	/// <remarks>
-	/// <para>
-	/// Assemblies are specified via an <see cref="AssemblySet"/>. Individual assemblies are specified by their identity information. Only a
-	/// name is required but, optionally, the assembly version, culture and public key token may be specified.
-	/// </para>
-	/// <para>
-	/// Assemblies can be uninstalled from the GAC with or without reference counting. The full details of reference counting can be found in
-	/// the SDK documentation.
-	/// </para>
-	/// </remarks>
-	/// <example>
-	///   <para>Uninstalls <c>Shared</c> assembly from the GAC.</para>
-	///   <code>
-	///     <![CDATA[
-	/// <gac-uninstall>
-	///		<assemblies>
-	///			<assembly name="Shared" />
-	///		</assemblies>
-	/// </gac-uninstall>
-	///     ]]>
-	/// </code>
-	/// </example>
-	/// <example>
-	///   <para>
-	///   Uninstalls <c>Shared</c> and <c>MyWeb</c> from the GAC.
-	///   </para>
-	///   <code>
-	///     <![CDATA[
-	/// <gac-uninstall>
-	///		<assemblies>
-	///			<assembly name="Shared" />
-	///			<assembly name="MyWeb" />
-	///		</assemblies>
-	/// </gac-uninstall>
-	///     ]]>
-	///   </code>
-	/// </example>
-	/// <example>
-	///   <para>
-	///   Decrements references to <c>Shared</c> in the GAC and uninstalls if 
-	///   the reference count reaches zero.
-	///   </para>
-	///   <code>
-	///     <![CDATA[
-	/// <gac-uninstall scheme-type="Opaque" scheme-id="MyID" scheme-description="My description">
-	///		<assemblies>
-	///			<assembly name="Shared" />
-	///		</assemblies>
-	/// </gac-uninstall>
-	///     ]]>
-	///   </code>
-	/// </example>
-	/// <example>
-	///   <para>
-	///   Uninstalls version <c>2.1.7.9201</c> of <c>Shared</c> plus the 
-	///   Australian-cultured <c>MyWeb</c> from the GAC.
-	///   </para>
-	///   <code>
-	///     <![CDATA[
-	/// <gac-uninstall>
-	///		<assemblies>
-	///			<assembly name="Shared" version="2.1.7.9201" />
-	///			<assembly name="MyWeb" culture="en-AU" />
-	///		</assemblies>
-	/// </gac-uninstall>
-	///     ]]>
-	///   </code>
-	/// </example>
-	/// <example>
-	///   <para>
-	///   Uninstalls the neutrally-cultured, version <c>1.0.5000.0</c> of 
-	///   <c>System.Xml</c> from the native image cache. The assembly must
-	///   also have a public key token of <c>b77a5c561934e08a</c> to be 
-	///   uninstalled.
-	///   </para>
-	///   <code>
-	///     <![CDATA[
-	/// <gac-uninstall native="true">
-	///		<assemblies>
-	///			<assembly name="System.Xml" version="1.0.5000.0" public-key-token="b77a5c561934e08a" culture="Neutral" />
-	///		</assemblies>
-	/// </gac-uninstall>
-	///     ]]>
-	///   </code>
-	/// </example>
-	[ProgramLocation(LocationType.FrameworkSdkDir)]
-	[TaskName("gac-uninstall")]
-	public sealed class GacUninstallTask : GacTaskBase {
-		#region Fields
+    /// <summary>
+    /// Uninstalls assemblies from the Global Assembly Cache (GAC) by using the <c>gacutil</c> SDK tool.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Assemblies are specified via an <see cref="AssemblySet"/>. Individual assemblies are specified by their identity information. Only a
+    /// name is required but, optionally, the assembly version, culture and public key token may be specified.
+    /// </para>
+    /// <para>
+    /// Assemblies can be uninstalled from the GAC with or without reference counting. The full details of reference counting can be found in
+    /// the SDK documentation.
+    /// </para>
+    /// </remarks>
+    /// <example>
+    ///   <para>Uninstalls <c>Shared</c> assembly from the GAC.</para>
+    ///   <code>
+    ///     <![CDATA[
+    /// <gac-uninstall>
+    ///        <assemblies>
+    ///            <assembly name="Shared" />
+    ///        </assemblies>
+    /// </gac-uninstall>
+    ///     ]]>
+    /// </code>
+    /// </example>
+    /// <example>
+    ///   <para>
+    ///   Uninstalls <c>Shared</c> and <c>MyWeb</c> from the GAC.
+    ///   </para>
+    ///   <code>
+    ///     <![CDATA[
+    /// <gac-uninstall>
+    ///        <assemblies>
+    ///            <assembly name="Shared" />
+    ///            <assembly name="MyWeb" />
+    ///        </assemblies>
+    /// </gac-uninstall>
+    ///     ]]>
+    ///   </code>
+    /// </example>
+    /// <example>
+    ///   <para>
+    ///   Decrements references to <c>Shared</c> in the GAC and uninstalls if 
+    ///   the reference count reaches zero.
+    ///   </para>
+    ///   <code>
+    ///     <![CDATA[
+    /// <gac-uninstall scheme-type="Opaque" scheme-id="MyID" scheme-description="My description">
+    ///        <assemblies>
+    ///            <assembly name="Shared" />
+    ///        </assemblies>
+    /// </gac-uninstall>
+    ///     ]]>
+    ///   </code>
+    /// </example>
+    /// <example>
+    ///   <para>
+    ///   Uninstalls version <c>2.1.7.9201</c> of <c>Shared</c> plus the 
+    ///   Australian-cultured <c>MyWeb</c> from the GAC.
+    ///   </para>
+    ///   <code>
+    ///     <![CDATA[
+    /// <gac-uninstall>
+    ///        <assemblies>
+    ///            <assembly name="Shared" version="2.1.7.9201" />
+    ///            <assembly name="MyWeb" culture="en-AU" />
+    ///        </assemblies>
+    /// </gac-uninstall>
+    ///     ]]>
+    ///   </code>
+    /// </example>
+    /// <example>
+    ///   <para>
+    ///   Uninstalls the neutrally-cultured, version <c>1.0.5000.0</c> of 
+    ///   <c>System.Xml</c> from the native image cache. The assembly must
+    ///   also have a public key token of <c>b77a5c561934e08a</c> to be 
+    ///   uninstalled.
+    ///   </para>
+    ///   <code>
+    ///     <![CDATA[
+    /// <gac-uninstall native="true">
+    ///        <assemblies>
+    ///            <assembly name="System.Xml" version="1.0.5000.0" public-key-token="b77a5c561934e08a" culture="Neutral" />
+    ///        </assemblies>
+    /// </gac-uninstall>
+    ///     ]]>
+    ///   </code>
+    /// </example>
+    [ProgramLocation(LocationType.FrameworkSdkDir)]
+    [TaskName("gac-uninstall")]
+    public sealed class GacUninstallTask : GacTaskBase {
+        #region Fields
 
-		/// <summary>
-		/// See <see cref="Native" />.
-		/// </summary>
-		private bool _native;
+        /// <summary>
+        /// See <see cref="Native" />.
+        /// </summary>
+        private bool _native;
 
-		/// <summary>
-		/// See <see cref="Assemblies" />.
-		/// </summary>
-		private AssemblySet _assemblies;
+        /// <summary>
+        /// See <see cref="Assemblies" />.
+        /// </summary>
+        private AssemblySet _assemblies;
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		/// <summary>
-		/// If <see langword="true" />, specifies that the assemblies should be 
-		/// uninstalled from the native image cache. The default is <see langword="false" />.
-		/// </summary>
-		[TaskAttribute("native", Required = false)]
-		[BooleanValidator]
-		public bool Native {
-			get { return _native; }
-			set { _native = value; }
-		}
+        /// <summary>
+        /// If <see langword="true" />, specifies that the assemblies should be 
+        /// uninstalled from the native image cache. The default is <see langword="false" />.
+        /// </summary>
+        [TaskAttribute("native", Required = false)]
+        [BooleanValidator]
+        public bool Native {
+            get { return _native; }
+            set { _native = value; }
+        }
 
-		/// <summary>
-		/// Specifies the assemblies to uninstall.
-		/// </summary>
-		[BuildElement("assemblies", Required=true)]
-		public AssemblySet Assemblies {
+        /// <summary>
+        /// Specifies the assemblies to uninstall.
+        /// </summary>
+        [BuildElement("assemblies", Required=true)]
+        public AssemblySet Assemblies {
             get { return _assemblies; }
-			set { _assemblies = value; }
-		}
+            set { _assemblies = value; }
+        }
 
-		/// <summary>
-		/// Gets the assembly list to uninstall.
-		/// </summary>
-		protected override ICollection AssemblyList {
-			get { return _assemblies.AssemblyCollection; }
-		}
+        /// <summary>
+        /// Gets the assembly list to uninstall.
+        /// </summary>
+        protected override ICollection AssemblyList {
+            get { return _assemblies.AssemblyCollection; }
+        }
 
-		/// <summary>
-		/// If <see langword="true" />, the specified assemblies will be forcibly 
-		/// removed from the GAC. All references to the specified assemblies will
-		/// be removed from the GAC prior to removing the assemblies themselves. 
-		/// The default is <see langword="false" />.
-		/// </summary>
-		/// <remarks>
-		/// You cannot use this option to remove an assembly that was installed using Microsoft Windows Installer.
-		/// </remarks>
+        /// <summary>
+        /// If <see langword="true" />, the specified assemblies will be forcibly 
+        /// removed from the GAC. All references to the specified assemblies will
+        /// be removed from the GAC prior to removing the assemblies themselves. 
+        /// The default is <see langword="false" />.
+        /// </summary>
+        /// <remarks>
+        /// You cannot use this option to remove an assembly that was installed using Microsoft Windows Installer.
+        /// </remarks>
         [TaskAttribute("force", Required=false)]
         [BooleanValidator]
-		public override bool Force {
-			get { return base.Force; }
-			set { base.Force = value; }
-		}
+        public override bool Force {
+            get { return base.Force; }
+            set { base.Force = value; }
+        }
 
-		#endregion
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Constructs an instance of the <c>GacUninstallTask</c>.
-		/// </summary>
-		public GacUninstallTask() {
-			_assemblies = new AssemblySet();
-		}
+        /// <summary>
+        /// Constructs an instance of the <c>GacUninstallTask</c>.
+        /// </summary>
+        public GacUninstallTask() {
+            _assemblies = new AssemblySet();
+        }
 
-		/// <summary>
-		/// Appends any install-specific arguments.
-		/// </summary>
-		/// <param name="sb"></param>
-		protected override void AppendProgramArguments(StringBuilder sb) {
-			sb.Append(" /u");
+        /// <summary>
+        /// Appends any install-specific arguments.
+        /// </summary>
+        /// <param name="sb"></param>
+        protected override void AppendProgramArguments(StringBuilder sb) {
+            sb.Append(" /u");
 
-			if (Native) {
-				sb.Append("ngen");
-			}
-		}
+            if (Native) {
+                sb.Append("ngen");
+            }
+        }
 
-		/// <summary>
-		/// Outputs log information.
-		/// </summary>
-		protected override void BeforeExecuteTask() {
-			Log(Level.Info, string.Format("Uninstalling '{0}' . . .", CurrentAssembly));
-		}
+        /// <summary>
+        /// Outputs log information.
+        /// </summary>
+        protected override void BeforeExecuteTask() {
+            Log(Level.Info, string.Format("Uninstalling '{0}' . . .", CurrentAssembly));
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
