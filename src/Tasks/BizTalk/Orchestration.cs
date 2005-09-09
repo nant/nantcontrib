f58c4ce531cec94b8a0be4f9d6f56d0c82112598
drieseng <gert.driesen@telenet.be>
2005-09-09 13:09:43 +0000

    Add support for enlisting and unenlisting orchestrations Added logging

--- src/Tasks/BizTalk/Orchestration.cs
+++ src/Tasks/BizTalk/Orchestration.cs
@@ -128,6 +128,17 @@ namespace NAnt.Contrib.Tasks.BizTalk {
             _actions.Add(stop);
         }
 
+        [BuildElement("enlist")]
+        public void AddEnlistAction(EnlistOrchestrationAction enlist) {
+            _actions.Add(enlist);
+        }
+
+        [BuildElement("unenlist")]
+        public void AddUnenlistAction(UnenlistOrchestrationAction unenlist) {
+            _actions.Add(unenlist);
+        }
+
+
         #endregion Public Instance Methods
 
         #region Private Static Methods
@@ -151,7 +162,42 @@ namespace NAnt.Contrib.Tasks.BizTalk {
         }
 
         public abstract class OrchestrationActionBase : Element, IOrchestrationAction {
+            #region Override implementation of Element
+
+            /// <summary>
+            /// Logs a message with the given priority.
+            /// </summary>
+            /// <param name="messageLevel">The message priority at which the specified message is to be logged.</param>
+            /// <param name="message">The message to be logged.</param>
+            /// <remarks>
+            /// The actual logging is delegated to the project.
+            /// </remarks>
+            public override void Log(Level messageLevel, string message) {
+                ((Task) Parent).Log(messageLevel, message);
+            }
+
+            /// <summary>
+            /// Logs a message with the given priority.
+            /// </summary>
+            /// <param name="messageLevel">The message priority at which the specified message is to be logged.</param>
+            /// <param name="message">The message to log, containing zero or more format items.</param>
+            /// <param name="args">An <see cref="object" /> array containing zero or more objects to format.</param>
+            /// <remarks>
+            /// The actual logging is delegated to the project.
+            /// </remarks>
+            public override void Log(Level messageLevel, string message, params object[] args) {
+                ((Task) Parent).Log(messageLevel, message, args);
+            }
+
+            #endregion Override implementation of Element
+
+            #region Public Instance Methods
+
             public abstract void Invoke(ManagementObject orchestration);
+
+            #endregion Public Instance Methods
+
+            #region Protected Instance Methods
 
             protected string GetName(ManagementObject orchestration) {
                 return (string) orchestration["Name"];
@@ -169,92 +215,8 @@ namespace NAnt.Contrib.Tasks.BizTalk {
                 // enlist the orchestration
                 orchestration.InvokeMethod("Enlist", inParams, null);
             }
-        }
 
-        /// <summary>
-        /// Stops the orchestration.
-        /// </summary>
-        /// <remarks>
-        /// If the status of the orchestration is <see cref="ServiceStatus.Bound" />,
-        /// <see cref="ServiceStatus.Unbound" /> or <see cref="ServiceStatus.Stopped" />,
-        /// then no further processing is done.
-        /// </remarks>
-        public class StopOrchestrationAction : OrchestrationActionBase {
-            /// <summary>
-            /// Specifies whether receive locations associated with this 
-            /// orchestration should be automatically disabled. The default
-            /// is <see langword="false" />.
-            /// </summary>
-            [TaskAttribute("autodisablereceivelocation")]
-            public bool AutoDisableReceiveLocation {
-                get { return _autoDisableReceiveLocation; }
-                set { _autoDisableReceiveLocation = value; }
-            }
-
-            /// <summary>
-            /// Specifies whether instances of this orchestration should be
-            /// automatically suspended. The default is <see langword="true" />.
-            /// </summary>
-            [TaskAttribute("autosuspendorchestrationinstance")]
-            public bool AutoSuspendOrchestrationInstance {
-                get { return _autoSuspendOrchestrationInstance; }
-                set { _autoSuspendOrchestrationInstance = value; }
-            }
-
-            #region Implementation of IOrchestrationAction
-
-            /// <summary>
-            /// Stops the orchestration.
-            /// </summary>
-            /// <param name="orchestration">The orchestration to stop.</param>
-            /// <remarks>
-            /// If the status of orchestration is <see cref="ServiceStatus.Bound" />,
-            /// <see cref="ServiceStatus.Unbound" /> or <see cref="ServiceStatus.Stopped" />,
-            /// then no further processing is done.
-            /// </remarks>
-            public override void Invoke(ManagementObject orchestration) {
-                // get name of the orchestration
-                string name = GetName(orchestration);
-
-                try {
-                    ServiceStatus status = GetStatus(orchestration);
-                    switch (status) {
-                        case ServiceStatus.Unbound:
-                            Log(Level.Verbose, "Orchestration \"{0}\" is not bound."
-                                + " Skipping.", name);
-                            break;
-                        case ServiceStatus.Bound:
-                            Log(Level.Verbose, "Orchestration \"{0}\" is not started."
-                                + " Skipping.", name);
-                            break;
-                        case ServiceStatus.Stopped:
-                            Log(Level.Verbose, "Orchestration \"{0}\" is already stopped."
-                                + " Skipping.", name);
-                            break;
-                        default:
-                            // set-up parameters to pass to method
-                            ManagementBaseObject inParams = orchestration.GetMethodParameters("Stop");
-                            inParams["AutoDisableReceiveLocationFlag"] = AutoDisableReceiveLocation ? 2 : 1;
-                            inParams["AutoSuspendOrchestrationInstanceFlag"] = AutoSuspendOrchestrationInstance ? 2 : 1;
-                            // stop the orchestration
-                            orchestration.InvokeMethod("Stop", inParams, null);
-                            break;
-                    }
-                } catch (Exception ex) {
-                    throw new BuildException(string.Format(CultureInfo.InvariantCulture,
-                        "Orchestration \"{0}\" could not be stopped.", name), 
-                        Location, ex);
-                }
-            }
-
-            #endregion Implementation of IOrchestrationAction
-
-            #region Private Instance Fields
-
-            private bool _autoDisableReceiveLocation;
-            private bool _autoSuspendOrchestrationInstance = true;
-
-            #endregion Private Instance Fields
+            #endregion Protected Instance Methods
         }
 
         /// <summary>
@@ -264,8 +226,9 @@ namespace NAnt.Contrib.Tasks.BizTalk {
         /// If the orchestration is not yet enlisted, then this will be done 
         /// first.
         /// </remarks>
-        [ElementName("start")]
         public class StartOrchestrationAction : OrchestrationActionBase {
+            #region Public Instance Properties
+
             /// <summary>
             /// Specifies whether receive locations associated with this 
             /// orchestration should be automatically enabled. The default is
@@ -300,11 +263,15 @@ namespace NAnt.Contrib.Tasks.BizTalk {
                 set { _autoStartSendPorts = value; }
             }
 
+            #endregion Public Instance Properties
+
             #region Implementation of IOrchestrationAction
 
             public override void Invoke(ManagementObject orchestration) {
                 // get name of the orchestration
                 string name = GetName(orchestration);
+
+                Log(Level.Verbose, "Starting orchestration \"{0}\"...", name);
 
                 try {
                     ServiceStatus status = GetStatus(orchestration);
@@ -331,6 +298,8 @@ namespace NAnt.Contrib.Tasks.BizTalk {
                             inParams["AutoStartSendPortsFlag"] = AutoStartSendPorts ? 2 : 1;
                             // start the orchestration
                             orchestration.InvokeMethod("Start", inParams, null);
+                            // log success
+                            Log(Level.Info, "Started orchestration \"{0}\"", name);
                             break;
                     }
                 } catch (Exception ex) {
@@ -347,6 +316,221 @@ namespace NAnt.Contrib.Tasks.BizTalk {
             private bool _autoEnableReceiveLocation;
             private bool _autoResumeOrchestrationInstance  = true;
             private bool _autoStartSendPorts = true;
+
+            #endregion Private Instance Fields
+        }
+
+        /// <summary>
+        /// Stops the orchestration.
+        /// </summary>
+        /// <remarks>
+        /// If the status of the orchestration is <see cref="ServiceStatus.Bound" />,
+        /// <see cref="ServiceStatus.Unbound" /> or <see cref="ServiceStatus.Stopped" />,
+        /// then no further processing is done.
+        /// </remarks>
+        public class StopOrchestrationAction : OrchestrationActionBase {
+            #region Public Instance Properties
+
+            /// <summary>
+            /// Specifies whether receive locations associated with this 
+            /// orchestration should be automatically disabled. The default
+            /// is <see langword="false" />.
+            /// </summary>
+            [TaskAttribute("autodisablereceivelocation")]
+            public bool AutoDisableReceiveLocation {
+                get { return _autoDisableReceiveLocation; }
+                set { _autoDisableReceiveLocation = value; }
+            }
+
+            /// <summary>
+            /// Specifies whether instances of this orchestration should be
+            /// automatically suspended. The default is <see langword="true" />.
+            /// </summary>
+            [TaskAttribute("autosuspendorchestrationinstance")]
+            public bool AutoSuspendOrchestrationInstance {
+                get { return _autoSuspendOrchestrationInstance; }
+                set { _autoSuspendOrchestrationInstance = value; }
+            }
+
+            #endregion Public Instance Properties
+
+            #region Implementation of IOrchestrationAction
+
+            /// <summary>
+            /// Stops the orchestration.
+            /// </summary>
+            /// <param name="orchestration">The orchestration to stop.</param>
+            /// <remarks>
+            /// If the status of orchestration is <see cref="ServiceStatus.Bound" />,
+            /// <see cref="ServiceStatus.Unbound" /> or <see cref="ServiceStatus.Stopped" />,
+            /// then no further processing is done.
+            /// </remarks>
+            public override void Invoke(ManagementObject orchestration) {
+                // get name of the orchestration
+                string name = GetName(orchestration);
+
+                Log(Level.Verbose, "Stopping orchestration \"{0}\"...", name);
+
+                try {
+                    ServiceStatus status = GetStatus(orchestration);
+                    switch (status) {
+                        case ServiceStatus.Unbound:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is not bound."
+                                + " Skipping.", name);
+                            break;
+                        case ServiceStatus.Bound:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is not started."
+                                + " Skipping.", name);
+                            break;
+                        case ServiceStatus.Stopped:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is already stopped."
+                                + " Skipping.", name);
+                            break;
+                        default:
+                            // set-up parameters to pass to method
+                            ManagementBaseObject inParams = orchestration.GetMethodParameters("Stop");
+                            inParams["AutoDisableReceiveLocationFlag"] = AutoDisableReceiveLocation ? 2 : 1;
+                            inParams["AutoSuspendOrchestrationInstanceFlag"] = AutoSuspendOrchestrationInstance ? 2 : 1;
+                            // stop the orchestration
+                            orchestration.InvokeMethod("Stop", inParams, null);
+                            // log success
+                            Log(Level.Info, "Stopped orchestration \"{0}\"", name);
+                            break;
+                    }
+                } catch (Exception ex) {
+                    throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                        "Orchestration \"{0}\" could not be stopped.", name), 
+                        Location, ex);
+                }
+            }
+
+            #endregion Implementation of IOrchestrationAction
+
+            #region Private Instance Fields
+
+            private bool _autoDisableReceiveLocation;
+            private bool _autoSuspendOrchestrationInstance = true;
+
+            #endregion Private Instance Fields
+        }
+
+        /// <summary>
+        /// Enlists the orchestration by creating its activation subscription.
+        /// </summary>
+        public class EnlistOrchestrationAction : OrchestrationActionBase {
+            #region Public Instance Properties
+
+            /// <summary>
+            /// Specifies the BizTalk host on which to enlist the orchestration.
+            /// </summary>
+            [TaskAttribute("host", Required=true)]
+            [StringValidator(AllowEmpty=false)]
+            public string Host {
+                get { return _host; }
+                set { _host = value; }
+            }
+
+            #endregion Public Instance Properties
+
+            #region Implementation of IOrchestrationAction
+
+            public override void Invoke(ManagementObject orchestration) {
+                // get name of the orchestration
+                string name = GetName(orchestration);
+
+                Log(Level.Verbose, "Enlisting orchestration \"{0}\"", name);
+
+                try {
+                    ServiceStatus status = GetStatus(orchestration);
+                    switch (status) {
+                        case ServiceStatus.Started:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is already started."
+                                + " Skipping.", name);
+                            break;
+                        case ServiceStatus.Stopped:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is already enlisted."
+                                + " Skipping.", name);
+                            break;
+                        default:
+                            // enlist the orchestration
+                            Enlist(orchestration, Host);
+                            // log success
+                            Log(Level.Info, "Enlisted orchestration \"{0}\"", name);
+                            break;
+                    }
+                } catch (Exception ex) {
+                    throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                        "Orchestration \"{0}\" could not be enlisted.", name), 
+                        Location, ex);
+                }
+            }
+
+            #endregion Implementation of IOrchestrationAction
+
+            #region Private Instance Fields
+
+            private string _host;
+
+            #endregion Private Instance Fields
+        }
+
+        /// <summary>
+        /// Unenlists the orchestration by removing its activation subscription.
+        /// </summary>
+        public class UnenlistOrchestrationAction : OrchestrationActionBase {
+            #region Public Instance Properties
+
+            /// <summary>
+            /// Specifies whether instances of this orchestration type should 
+            /// be automatically terminated. The default is <see langword="false" />.
+            /// </summary>
+            [TaskAttribute("autoterminateorchestrationinstance")]
+            public bool AutoTerminateOrchestrationInstance {
+                get { return _autoTerminateOrchestrationInstance; }
+                set { _autoTerminateOrchestrationInstance = value; }
+            }
+
+            #endregion Public Instance Properties
+
+            #region Implementation of IOrchestrationAction
+
+            public override void Invoke(ManagementObject orchestration) {
+                // get name of the orchestration
+                string name = GetName(orchestration);
+
+                Log(Level.Verbose, "Unenlisting orchestration \"{0}\"...", name);
+
+                try {
+                    ServiceStatus status = GetStatus(orchestration);
+                    switch (status) {
+                        case ServiceStatus.Bound:
+                        case ServiceStatus.Unbound:
+                            Log(Level.Verbose, "Orchestration \"{0}\" is not enlisted."
+                                + " Skipping.", name);
+                            break;
+                        case ServiceStatus.Started:
+                        case ServiceStatus.Stopped:
+                            // set-up parameters to pass to method
+                            ManagementBaseObject inParams = orchestration.GetMethodParameters("Unenlist");
+                            inParams["AutoTerminateOrchestrationInstanceFlag"] = AutoTerminateOrchestrationInstance ? 2 : 1;
+                            // unenlist the orchestration
+                            orchestration.InvokeMethod("Unenlist", inParams, null);
+                            // log success
+                            Log(Level.Info, "Unenlisted orchestration \"{0}\"", name);
+                            break;
+                    }
+                } catch (Exception ex) {
+                    throw new BuildException(string.Format(CultureInfo.InvariantCulture,
+                        "Orchestration \"{0}\" could not be unenlisted.", name), 
+                        Location, ex);
+                }
+            }
+
+            #endregion Implementation of IOrchestrationAction
+
+            #region Private Instance Fields
+
+            private bool _autoTerminateOrchestrationInstance;
 
             #endregion Private Instance Fields
         }
