61f1532fd77f74fa7bcdfb8b286de159408c3478
Ian MacLean <ianm74@users.sourceforge.net>
2005-10-05 06:32:53 +0000

    Frist commit of a source safe Delete task. From Richard Adleta

---
+++ src/Tasks/SourceSafe/DeleteTask.cs
@@ -0,0 +1,127 @@
+#region GNU General Public License
+//
+// NAntContrib
+// Copyright (C) 2001-2003 Gerry Shaw
+//
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// Richard Adleta (richardadleta@yahoo.com)
+#endregion
+
+using System;
+using System.IO;
+
+using SourceSafeTypeLib;
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace NAnt.Contrib.Tasks.SourceSafe {
+    /// <summary>
+    /// Used to delete or destory files or projects in Visual Source Safe.
+    /// </summary>
+    /// <example>
+    ///   <para>Delete a project from a local sourcesafe database.</para>
+    ///   <code><![CDATA[
+    ///     <vssdelete 
+    ///       user="myusername" 
+    ///       password="mypassword" 
+    ///       dbpath="C:\VSS\srcsafe.ini"
+    ///       path="$/MyProduct"
+    ///     />
+    ///   ]]></code>
+    /// </example>
+    /// <example>
+    ///   <para>Delete a file from the remote sourcesafe database.</para>
+    ///   <code><![CDATA[
+    ///     <vsscheckin 
+    ///       user="myusername" 
+    ///       password="mypassword" 
+    ///       dbpath="\\MyServer\VSS\srcsafe.ini"
+    ///       path="$/MyProduct/myFile.cs"
+    ///     />
+    ///   ]]></code>
+    /// </example>
+    /// <example>
+    ///   <para>Destory a project from a local sourcesafe database.</para>
+    ///   <code><![CDATA[
+    ///     <vssdelete 
+    ///       user="myusername" 
+    ///       password="mypassword" 
+    ///       dbpath="C:\VSS\srcsafe.ini"
+    ///       path="$/MyProduct"
+    ///       destory="true"
+    ///     />
+    ///   ]]></code>
+    /// </example>
+    /// <example>
+    ///   <para>Destroy a file from the remote sourcesafe database.</para>
+    ///   <code><![CDATA[
+    ///     <vssdelete 
+    ///       user="myusername" 
+    ///       password="mypassword" 
+    ///       dbpath="\\MyServer\VSS\srcsafe.ini"
+    ///       path="$/MyProduct/myFile.cs"
+    ///       destory="true"
+    ///     />
+    ///   ]]></code>
+    /// </example>
+    [TaskName("vssdelete")]
+    public sealed class DeleteTask : BaseTask {
+        #region Private Instance Fields
+        
+        private bool _destroy = false;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// Determines whether or not the item is destroyed. 
+        /// The default is <see langword="false" />.
+        /// </summary>
+        [TaskAttribute("destroy")]
+        [BooleanValidator()]
+        public bool Destroy { 
+            get { return _destroy; }
+            set { _destroy = value; }
+        }
+        
+        #endregion Public Instance Properties
+
+        protected override void ExecuteTask() {
+            Open();
+
+            if( _destroy ) {
+                try {
+                    Item.Destroy();
+                    Log(Level.Info, "Destroyed '{0}'.", Path);
+                } catch (Exception ex) {
+                    throw new BuildException("The destroy operation failed.", 
+                        Location, ex);
+                }
+            } else {
+                try {
+                    Item.Deleted=true;
+                    Log(Level.Info, "Deleted '{0}'.", Path);
+                } catch (Exception ex) {
+                    throw new BuildException("The delete operation failed.", 
+                        Location, ex);
+                }
+            }
+        }
+    }
+}
