26f4b7881f6d68c75cbbc85d65c15912038297ca
drieseng <gert.driesen@telenet.be>
2006-04-08 11:59:21 +0000

    If no machine is specified, then default to local computer. Added get-status function => requires latest nightly build of NAnt.

--- src/Functions/ServiceFunctions.cs
+++ src/Functions/ServiceFunctions.cs
@@ -21,14 +21,15 @@ using System;
 using System.ServiceProcess;
 
 using NAnt.Core;
-using NAnt.Core.Types;
 using NAnt.Core.Attributes;
+using NAnt.Core.Types;
+using NAnt.Core.Util;
 
 namespace NAnt.Contrib.Functions {
     /// <summary>
     /// Allow information on a Windows service to be retrieved.
     /// </summary>
-    [FunctionSet("service", "Services")]
+    [FunctionSet("service", "Windows Service")]
     public class ServiceFunctions : FunctionSetBase {
         #region Public Instance Constructors
         
@@ -43,14 +44,31 @@ namespace NAnt.Contrib.Functions {
         /// installed on a given machine.
         /// </summary>
         /// <param name="service">The short name that identifies the service to the system.</param>
-        /// <param name="machine">The computer on which the service resides.</param>
+        /// <param name="machineName">The computer on which the service resides.</param>
         /// <returns>
         /// <see langword="true" /> if the service is installed; otherwise,
         /// <see langword="false" />.
         /// </returns>
+        /// <remarks>
+        /// For the machineName parameter, you can use "." or a zero-length
+        /// <see cref="string" /> to represent the local computer.
+        /// </remarks>
+        /// <example>
+        ///   <para>
+        ///   The following example starts the "World Wide Web Publishing"
+        ///   service if it's installed on the local computer.
+        ///   </para>
+        ///   <code>
+        ///     <![CDATA[
+        /// <if test="${service::is-installed('World Wide Web Publishing', '.')}">
+        ///     <servicecontroller action="Start" service="World Wide Web Publishing" />
+        /// </if>
+        ///     ]]>
+        ///   </code>
+        /// </example>
         [Function("is-installed")]
-        public static bool IsInstalled(string service, string machine) {
-            using (ServiceController sc = new ServiceController(service, machine)) {
+        public static bool IsInstalled(string service, string machineName) {
+            using (ServiceController sc = GetServiceController(service, machineName)) {
                 try {
                     bool isStarted = (sc.Status == ServiceControllerStatus.Running);
                     return true;
@@ -64,14 +82,18 @@ namespace NAnt.Contrib.Functions {
         /// Returns a value indicating whether the specified service is running.
         /// </summary>
         /// <param name="service">The short name that identifies the service to the system.</param>
-        /// <param name="machine">The computer on which the service resides.</param>
+        /// <param name="machineName">The computer on which the service resides.</param>
         /// <returns>
         /// <see langword="true" /> if the service is running; otherwise,
         /// <see langword="false" />.
         /// </returns>
+        /// <remarks>
+        /// For the machineName parameter, you can use "." or a zero-length
+        /// <see cref="string" /> to represent the local computer.
+        /// </remarks>
         [Function("is-running")]
-        public static bool IsRunning(string service, string machine){
-            using (ServiceController sc = new ServiceController(service, machine)) {
+        public static bool IsRunning(string service, string machineName) {
+            using (ServiceController sc = GetServiceController(service, machineName)) {
                 return sc.Status == ServiceControllerStatus.Running;
             }
         }
@@ -80,14 +102,18 @@ namespace NAnt.Contrib.Functions {
         /// Returns a value indicating whether the specified service is stopped.
         /// </summary>
         /// <param name="service">The short name that identifies the service to the system.</param>
-        /// <param name="machine">The computer on which the service resides.</param>
+        /// <param name="machineName">The computer on which the service resides.</param>
         /// <returns>
         /// <see langword="true" /> if the service is stopped; otherwise,
         /// <see langword="false" />.
         /// </returns>
+        /// <remarks>
+        /// For the machineName parameter, you can use "." or a zero-length
+        /// <see cref="string" /> to represent the local computer.
+        /// </remarks>
         [Function("is-stopped")]
-        public static bool IsStopped(string service, string machine){
-            using (ServiceController sc = new ServiceController(service, machine)) {
+        public static bool IsStopped(string service, string machineName) {
+            using (ServiceController sc = GetServiceController(service, machineName)) {
                 return sc.Status == ServiceControllerStatus.Stopped;
             }
         }
@@ -96,18 +122,95 @@ namespace NAnt.Contrib.Functions {
         /// Returns a value indicating whether the specified service is paused.
         /// </summary>
         /// <param name="service">The short name that identifies the service to the system.</param>
-        /// <param name="machine">The computer on which the service resides.</param>
+        /// <param name="machineName">The computer on which the service resides.</param>
         /// <returns>
         /// <see langword="true" /> if the service is paused; otherwise,
         /// <see langword="false" />.
         /// </returns>
+        /// <remarks>
+        /// For the machineName parameter, you can use "." or a zero-length
+        /// <see cref="string" /> to represent the local computer.
+        /// </remarks>
         [Function("is-paused")]
-        public static bool IsPaused(string service, string machine){
-            using (ServiceController sc = new ServiceController(service, machine)) {
+        public static bool IsPaused(string service, string machineName) {
+            using (ServiceController sc = GetServiceController(service, machineName)) {
                 return sc.Status == ServiceControllerStatus.Paused;
             }
         }
 
+        /// <summary>
+        /// Gets the status of the service.
+        /// </summary>
+        /// <param name="service">The short name that identifies the service to the system.</param>
+        /// <param name="machineName">The computer on which the service resides.</param>
+        /// <returns>
+        /// One of the <see cref="ServiceControllerStatus" /> values that
+        /// indicates whether the service is running, stopped, or paused, or
+        /// whether a start, stop, pause, or continue command is pending.
+        /// </returns>
+        /// <remarks>
+        ///   <para>
+        ///   For the machineName parameter, you can use "." or a zero-length
+        ///   <see cref="string" /> to represent the local computer.
+        ///   </para>
+        ///   <para>
+        ///   The value returned by <see cref="GetStatus" /> can be compared 
+        ///   to either a corresponding enum field name or the underlying
+        ///   integral value.
+        ///   </para>
+        /// </remarks>
+        /// <example>
+        ///   <para>
+        ///   Displays a warning if the <b>Alerter</b> service is stopping
+        ///   on <c>SV-ARD-EAI1</c>.
+        ///   </para>
+        ///   <code>
+        ///     <![CDATA[
+        /// <if test="${service::get-status('Alerter', 'SV-ARD-EAI1') == 'StopPending'}">
+        ///     <echo level="Warning">The Alerter service is stopping.</echo>
+        /// </if>
+        ///     ]]>
+        ///   </code>
+        /// </example>
+        /// <example>
+        ///   <para>
+        ///   The &quot;deploy-web-application&quot; target is only executed if
+        ///   IIS is running on the local computer.
+        ///   </para>
+        ///   <code>
+        ///     <![CDATA[
+        /// <target name="deploy" depends="deploy-sql-scripts, deploy-web-application" />
+        /// 
+        /// <target name="deploy-sql-scripts">
+        ///     ...
+        /// </target>
+        /// 
+        /// <target name="deploy-web-application" if="$(service::get-status('World Wide Web Publishing', '.') == 4)}">
+        ///     ...
+        /// </target>
+        ///     ]]>
+        ///   </code>
+        /// </example>
+        [Function("get-status")]
+        public static ServiceControllerStatus GetStatus(string service, string machineName) {
+            using (ServiceController sc = GetServiceController(service, machineName)) {
+                return sc.Status;
+            }
+        }
+
+
         #endregion Public Instance Methods
+
+        #region Private Static Methods
+
+        private static ServiceController GetServiceController(string service, string machineName) {
+            if (StringUtils.IsNullOrEmpty(machineName)) {
+                machineName = ".";
+            }
+            
+            return new ServiceController(service, machineName);
+        }
+
+        #endregion Private Static Methods
     }
 }
