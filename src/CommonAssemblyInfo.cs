46e970b36c210d6d717def76ab799e346f637d36
Ryan Boggs <rmboggs@gmail.com>
2012-04-22 21:04:29 -0700

    Committing files for 0.92 Alpha1 release.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -10,12 +10,12 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyConfigurationAttribute("release")]
+[assembly: AssemblyConfigurationAttribute("alpha1")]
 [assembly: AssemblyCompanyAttribute("http://nantcontrib.sourceforge.net")]
 [assembly: AssemblyProductAttribute("NAntContrib")]
-[assembly: AssemblyCopyrightAttribute("Copyright (C) 2001-2011 Gerry Shaw")]
+[assembly: AssemblyCopyrightAttribute("Copyright (C) 2001-2012 Gerry Shaw")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("0.91.4312.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.91")]
+[assembly: AssemblyVersionAttribute("0.92.4495.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.92")]
 
